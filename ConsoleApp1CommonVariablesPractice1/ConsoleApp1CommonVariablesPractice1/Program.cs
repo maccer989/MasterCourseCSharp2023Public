f8bc109583698251441ba7014d683d75f1638293
Ian McNerlin <IanMcNerlin@hotmail.co.uk>
2023-05-07 11:48:58 +0100

    Common variables practice1
    Common variables practice1

---
+++ ConsoleApp1CommonVariablesPractice1/ConsoleApp1CommonVariablesPractice1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Common variables practice1
+
+string firstName = string.Empty;
+string lastName = string.Empty;
+bool isAlive = false;
+string telNumber = string.Empty;
+
+Console.Write("Enter your first name:");
+firstName = Console.ReadLine();
+Console.Write("Enter your last name:");
+lastName = Console.ReadLine();
+Console.Write("Are you alive enter yes/no:");
+isAlive= Console.ReadLine() == "yes";
+Console.Write("Enter your telephone number:");
+telNumber = Console.ReadLine();
+
+Console.WriteLine($"Hello {firstName} {lastName}, you are alive:{isAlive}, your telephone number is:{telNumber}");
+

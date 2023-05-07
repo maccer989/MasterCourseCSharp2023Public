00f0af85ea15716506dfc70a03e2c6b444efe890
Ian McNerlin <IanMcNerlin@hotmail.co.uk>
2023-05-07 13:54:54 +0100

    Common Variables Practice2
    Common Variables Practice2

---
+++ ConsoleApp1CommonVariablesPractice2/ConsoleApp1CommonVariablesPractice2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Common Variables Practice2
+
+
+//Create a console application that captures a user's age then identify
+//how old they will be in 25 years,
+//as well as how old they were 25 years ago. Print that information to the Console
+
+Console.Write("What is your age: ");
+string? ageText = Console.ReadLine();
+bool isValidInt = int.TryParse(ageText, out int age);
+
+Console.WriteLine($"The age was {age} and is valid.");
+Console.WriteLine($"You will be {age + 25} in 25 years.");
+Console.WriteLine($"You were {age -25}, 25 years ago.");

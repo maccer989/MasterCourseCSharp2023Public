f61ec39a9f1e77a12eb274a6e7798b62484e6aaf
Ian McNerlin <IanMcNerlin@hotmail.co.uk>
2023-05-08 14:09:35 +0100

    Conditionals Practice1
    Conditionals Practice1

---
+++ ConsoleApp1ConditionalsPractice1/ConsoleApp1ConditionalsPractice1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Conditionals Practice1
+
+//Create a console application that asks the suer for theri name .Welcome yourself (insert name e.g. david) as Professor
+//anyone else is student. Maek sure that capitalised version of your name 'DAVID' is also welcomed as Professor
+
+Console.Write("Please enter your name:");
+string? firstName = Console.ReadLine();
+
+if(firstName.ToLower() == "david")
+{
+    Console.WriteLine("Welcome Professor");
+}
+
+else
+{
+    Console.WriteLine("Welcome Student");
+}
+
+
+Console.Write("End of program");

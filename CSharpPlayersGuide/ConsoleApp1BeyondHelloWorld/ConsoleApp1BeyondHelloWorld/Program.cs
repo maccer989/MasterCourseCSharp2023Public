091df0c1240c560d41681015fa5058c2a7ec5c65
maccer989 <132687553+maccer989@users.noreply.github.com>
2023-06-04 19:06:19 +0100

    More Hello World
    More Hello World

---
+++ CSharpPlayersGuide/ConsoleApp1BeyondHelloWorld/ConsoleApp1BeyondHelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine( "How are things?");
+Console.WriteLine( "Things are okay and how are you?");
+Console.WriteLine("Have you been busy today?");
+Console.WriteLine( "Yes I have thanks.");
+
+Console.ReadLine();
